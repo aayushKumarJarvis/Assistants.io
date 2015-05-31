9d993df260b9c82fc49ebe6fb3c472f14d92315d
Aayush Kumar <iaayush.stevejobs@gmail.com>
2015-05-31 18:21:59 +0530

    Fixing the bug

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,23 @@
 
 $(function() {
     $(".submit_button").click(function() {
-        sendDataToServer();
-    });
-});
-
-function sendDataToServer() {
-
-    $.ajax({
-        url:'send_email.php',
-        data :{
-            email:$("#email").val(),
-            name : $("#name").val(),
-            message : $("#message").val()
-        },
-        type :"POST",
-        success: function() {
+        $.ajax({
+            url:'send_email.php',
+            data :{
+                email:$("#email").val(),
+                name : $("#name").val(),
+                message : $("#message").val()
+            },
+            type :"POST",
+            success: function() {
                 $('.contact_form').html("<div id='message_after'></div>");
                 $('#message_after').html("<h2>Your response has been submitted!</h2>").append("<p>We will be in touch soon.</p>");
-        },
-        error: function() {
-          alert("Something is wrong in your form. Please check the details again.");
-        }
-    });
+            },
+            error: function() {
+                alert("Something is wrong in your form. Please check the details again.");
+            }
+        });
 
-}
+        return false;
+    });
+});
